efc6f81dd0cdadc8a654d23a66871b91919c4bce
Neto Bonato <joaoadolfobonato@gmail.com>
2025-04-27 04:02:42 -0300

    convertendo projeto para typescript

--- routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { GenerateArchiveController } from './controller.js';
-
-const router = Router();
-
-router.get('/generate-archive', new GenerateArchiveController().handle);
-
-export { router };
