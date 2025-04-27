7234b438c1571c6cd585aa14fa6f9313ef28c742
Neto Bonato <joaoadolfobonato@gmail.com>
2025-04-27 03:27:50 -0300

    convertendo retorno da api de email em xlsx

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import { router } from './routes.js';
+
+const app = express()
+const HOST = 4000
+
+app.use(express.json())
+app.use(router)
+app.use(cors())
+
+app.use((err, req, res, next) => {
+  if(err instanceof Error) {
+    return res.status(400).json({
+      error: err.message
+    })
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error.'
+  })
+})
+
+app.listen(HOST, () => {
+  console.log(`Servidor rodando na porta ${HOST}`)
+})
--- service.js
+++
@@ -1,5 +0,0 @@
-class GenerateArchiveService {
-  async execute(params) {
-    
-  }
-}
